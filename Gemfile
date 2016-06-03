95b7269b4d45108fafe9e02d611440f131d5dd36
Rachel Prows <rachel.prows@icloud.com>
2016-06-03 12:15:59 -0600

    pagination

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 gem 'materialize-sass'
+gem 'will_paginate', '~> 3.1.0'
 
 group :production do
 gem 'puma'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-  	@animals = Animal.all
+  	#BAD - Slow response time
+  	#@animals = Animal.all
+  	@animals = Animal.paginate(page: params[:page], per_page: 10)
   end
 end
